ea079353f7ca8f7ef5376acd73f0a1c5fd484eca
Samuel <samawyck@gmail.com>
2025-10-10 17:37:22 -0500

    Create project db

---
+++ src/db/projects.js
@@ -0,0 +1,19 @@
+import beakBookImg from "../assets/projectImages/beakbook.png";
+
+
+const projects = [
+    {
+        title: "Beakbook",
+        bannerImg: beakBookImg,
+        description: "A messaging web application akin to discord. Users can send chat messages, friend requests, and create chat groups in real time.",
+        details: "I used web sockets for real-time chatting and notifications. The most challenging part ",
+        projectLink: "https://beakbook.pages.dev/",
+        gitHubLink: "https://github.com",
+        // downLoadLink: "", 
+        techUsed: ["Socket.io", "React.js", "Express.js", "postgreSQL", "Passport.js"]
+    }
+];
+
+
+
+export default projects;
