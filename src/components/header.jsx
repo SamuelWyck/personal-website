80c41068c56f1b0c13abc330127c88b00875e78d
Samuel <samawyck@gmail.com>
2025-10-10 11:30:50 -0500

    Use toolTipWrapper for tool tips

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import "../styles/header.css";
 import linkedinImg from "../assets/linkedin.svg";
 import githubImg from "../assets/github.svg";
+import ToolTipWrapper from "./toolTipWrapper";
 
 
 
@@ -11,15 +12,21 @@ function Header() {
             <div className="header-backing"></div>
             <p className="header-title">Samuel Wycklendt</p>
             <nav className="header-nav">
-                <a href="https://www.linkedin.com/in/swycklendt/">
-                    <img src={linkedinImg} alt="linkedin" />
-                    <p>LinkedIn</p>
-                </a>
-                <a href="https://github.com/SamuelWyck?tab=repositories">
-                    <img src={githubImg} alt="github" />
-                    <p>GitHub</p>
-                </a>
-                <a href="#">Blog</a>
+                <ToolTipWrapper toolTip={"My LinkedIn"}>
+                    <a href="https://www.linkedin.com/in/swycklendt/">
+                        <img src={linkedinImg} alt="linkedin" />
+                        <p>LinkedIn</p>
+                    </a>
+                </ToolTipWrapper>
+                <ToolTipWrapper toolTip={"My GitHub"}>
+                    <a href="https://github.com/SamuelWyck?tab=repositories">
+                        <img src={githubImg} alt="github" />
+                        <p>GitHub</p>
+                    </a>
+                </ToolTipWrapper>
+                <ToolTipWrapper toolTip={"My Blog"}>
+                    <a href="#">Blog</a>
+                </ToolTipWrapper>
             </nav>
         </header>
     );
