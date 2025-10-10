b4c0675be899349d24367791dee5959c0572499d
Samuel <samawyck@gmail.com>
2025-10-10 11:05:25 -0500

    Hook up header to App element

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Header from './components/header.jsx';
 
 
 
 function App() {
 	return (
+		<>
+		<Header />
 		<Outlet />
+		<p></p>
+		</>
 	);
 };
 
