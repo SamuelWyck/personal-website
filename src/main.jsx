7e2a6ba60ba9ba051b13fded100c636352d0eac3
Samuel <samawyck@gmail.com>
2025-10-10 11:06:51 -0500

    Remove unneeded import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './router.jsx';
 import './index.css';
-import App from './App.jsx';
 
 
 
