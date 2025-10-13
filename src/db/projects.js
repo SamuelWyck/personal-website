c6a84023e647d14834af28173e9b3532bff5b646
Samuel <samawyck@gmail.com>
2025-10-13 09:40:37 -0500

    Add project details for beakbook project

--- src/db/projects.js
+++ src/db/projects.js
@@ -6,7 +6,7 @@ const projects = [
         title: "Beakbook",
         bannerImg: beakBookImg,
         description: "A messaging web application akin to discord. Users can send chat messages, friend requests, and create chat groups in real time.",
-        details: "I used web sockets for real-time chatting and notifications. The most challenging part ",
+        details: "Web sockets were used for real-time chatting and notifications. All connections are managed to allow for users to create, join, and leave group chats. Users can also invite friends to any group chat.",
         projectLink: "https://beakbook.pages.dev/",
         gitHubLink: "https://github.com",
         // downLoadLink: "", 
