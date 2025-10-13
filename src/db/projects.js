325c3bb17eb9d17b02a897c1f38ed1fd15c6c8b3
Samuel <samawyck@gmail.com>
2025-10-13 13:07:34 -0500

    Add more projects

--- src/db/projects.js
+++ src/db/projects.js
@@ -1,5 +1,7 @@
 import beakBookImg from "../assets/projectImages/beakbook.png";
 import owlRoostImg from "../assets/projectImages/owlroost.png";
+import waldoImg from "../assets/projectImages/waldo.png";
+import warpCommanderImg from "../assets/projectImages/warpCommander.png";
 
 
 
@@ -22,6 +24,24 @@ const projects = [
         projectLink: "https://owlroost.pages.dev/",
         gitHubLink: "https://github.com/SamuelWyck/owlroost-api",
         techUsed: ["React.js", "Express.js", "postgreSQL", "Passport.js"]
+    },
+    {
+        title: "Warp Commander",
+        bannerImg: warpCommanderImg,
+        description: "A retro single player game published on Steam. Players play as a spaceship and fight off waves of enemies and bosses. Players who make it far enough will add their score to the leaderboard.",
+        details: "This project was a challange to myself to make and publish a game without using a game engine. It was a solo dev project made purely in python using the pygame library.",
+        projectLink: "https://www.youtube.com/watch?v=RFcaEz8wLWc",
+        downloadLink: "https://store.steampowered.com/app/3840360/Warp_Commander/",
+        techUsed: ["Python", "Pygame"]
+    },
+    {
+        title: "Where's Waldo",
+        bannerImg: waldoImg,
+        description: "An image tagging web application in the form of a Where's Waldo game. Players must race to find Waldo and his friends. The fastest players will make it on to the leaderboard.",
+        details: "Player time and player guesses are validated on the server side so it is impossible to cheat. Leaderboard names are also validated to ensure that every name is unique.",
+        projectLink: "https://wheres-waldo-949.pages.dev/",
+        gitHubLink: "https://github.com/SamuelWyck/wheres-waldo-api",
+        techUsed: ["React.js", "Express.js", "postgreSQL", "Prisma ORM"]
     }
 ];
 
