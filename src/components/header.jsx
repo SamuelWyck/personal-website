a740d85eada3811f48b89482e479ea9eacaa2a2e
Samuel <samawyck@gmail.com>
2026-05-31 06:54:26 -0500

    Rework header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import "../styles/header.css";
-import linkedinImg from "../assets/linkedin.svg";
+// import linkedinImg from "../assets/linkedin.svg";
 import githubImg from "../assets/github.svg";
-import blogImg from "../assets/post.svg";
+// import blogImg from "../assets/post.svg";
 import ToolTipWrapper from "./toolTipWrapper.jsx";
 
 
@@ -13,24 +13,24 @@ function Header() {
             <div className="header-backing"></div>
             <h1 className="header-title">Samuel Wycklendt</h1>
             <nav className="header-nav">
-                <ToolTipWrapper toolTip={"My LinkedIn"}>
+                {/* <ToolTipWrapper toolTip={"My LinkedIn"}>
                     <a href="https://www.linkedin.com/in/swycklendt/">
                         <img src={linkedinImg} alt="linkedin" />
                         <p>LinkedIn</p>
                     </a>
-                </ToolTipWrapper>
+                </ToolTipWrapper> */}
                 <ToolTipWrapper toolTip={"My GitHub"}>
                     <a href="https://github.com/SamuelWyck?tab=repositories">
                         <img src={githubImg} alt="github" />
                         <p>GitHub</p>
                     </a>
                 </ToolTipWrapper>
-                <ToolTipWrapper toolTip={"My Blog"}>
+                {/* <ToolTipWrapper toolTip={"My Blog"}>
                     <a href="https://odin-blog-site.pages.dev/posts">
                         <img src={blogImg} alt="blog" />
                         <p>Blog</p>
                     </a>
-                </ToolTipWrapper>
+                </ToolTipWrapper> */}
             </nav>
         </header>
     );
